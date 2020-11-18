b0b0f6c31953a557ce39ec0be2feb32cc6eb7134
Guillaume <72020675+kaa-serpent@users.noreply.github.com>
2020-11-18 21:20:20 +0100

    Update synchronize_two_git_repo.sh

--- synchronize_two_git_repo.sh
+++ synchronize_two_git_repo.sh
@@ -4,16 +4,16 @@ repo1="https://github.com/kaa-serpent/script_synchronise_two_repo_git"
 repo2="https://github.com/kaa-serpent/script_synchronise_two_repo_git"
 
 echo ""
-if [[ "$PWD" = */db-planr ]]; then
+if [[ "$PWD" = */script_synchronize_two_repo_git ]]; then
   cd ../
   echo folder exist
 else
   echo folder do not exist, cloning folder ...
-  git clone ${repoHub}
+  git clone ${repo1}
 fi
 
 cd "$PWD"/db-planr
-git remote add lab ${repoLab}
+git remote add lab ${repo2}
 echo "****** Pull gitHub ******"
 git pull origin master
 echo "******************************************"
